2f9b871aee4dea6aae55373395c76d06fc6f3f4b
John <jnaglick@gmail.com>
2014-04-06 19:52:17 -0400

    fight.logs instead of fight.fight_logs

--- app/models/fight.rb
+++ app/models/fight.rb
@@ -2,7 +2,7 @@ class Fight < ActiveRecord::Base
   has_many :fight_entries
   has_many :mechs, through: :fight_entries
 
-  has_many :fight_logs, -> { order 'sequence ASC' }
+  has_many :logs, -> { order 'sequence ASC' }, class_name: 'FightLog'
 
   validates_presence_of :start_time
 end
