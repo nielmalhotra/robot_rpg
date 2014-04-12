5c88289cea8b0e57ac1c4e0543dc9853fb836a02
Niel Malhotra <nielmalhotra89@gmail.com>
2014-04-12 19:20:16 -0400

    Reset form after creating message or mech

--- app/assets/javascripts/mechs/mech_controller.js
+++ app/assets/javascripts/mechs/mech_controller.js
@@ -2,6 +2,7 @@ angular.module('robot_rpg')
 .controller('MechController', ['$scope', '$http', '$mechs', function($scope, $http, $mechs) {
     $scope.create_mech = function(mech) {
         $mechs.save(mech, function(data){
+            $scope.newmech = angular.copy({});
             $scope.load_mechs();
             $('#ng-notice').html(data.success);
         });
--- app/assets/javascripts/messages/message_controller.js
+++ app/assets/javascripts/messages/message_controller.js
@@ -2,6 +2,7 @@ angular.module('robot_rpg')
 .controller('MessageController', ['$scope', '$http', '$messages', function($scope, $http, $messages) {
     $scope.create_message = function(msg) {
         $messages.save(msg, function(data){
+            $scope.newmsg = angular.copy({});
             $scope.load_messages();
             $('#ng-notice').html(data.success);
         });
