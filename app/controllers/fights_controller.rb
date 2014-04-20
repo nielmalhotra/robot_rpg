f2ea7c9df7227543967b7ea7f24065da224abf60
John <jnaglick@gmail.com>
2014-04-20 12:20:20 -0400

    not needed with rescue_from in app controller

--- app/controllers/fights_controller.rb
+++ app/controllers/fights_controller.rb
@@ -42,15 +42,11 @@ class FightsController < ApplicationController
   end
 
   def create
-    begin
-      fight = current_user.create_fight(Mech.find_by_name(params[:my_mech]))
-      fight.invite_user(User.find_by_name(params[:opponent]))
-      if fight.valid?
-        render json: {success: 'Fight Created! Huaaah!'}
-      else
-        render json: {fail: 'Fight Not Created. Do it better.'}, status: 400
-      end
-    rescue
+    fight = current_user.create_fight(Mech.find_by_name(params[:my_mech]))
+    fight.invite_user(User.find_by_name(params[:opponent]))
+    if fight.valid?
+      render json: {success: 'Fight Created! Huaaah!'}
+    else
       render json: {fail: 'Fight Not Created. Do it better.'}, status: 400
     end
   end
